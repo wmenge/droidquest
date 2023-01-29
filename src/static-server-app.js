f767faaab8a8a1b9f3c6d314d14dc08fd5021d3e
wilco menge <wilcomenge@gmail.com>
2023-01-29 14:43:53 +0100

    some random commit

--- src/static-server-app.js
+++ src/static-server-app.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public/'))
+app.use(express.static(__dirname + '/public'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
